1940a81fc6cb6c0fbf8f3c06797732063b122fd3
Chris Colvin <chris.colvin@getinnexus.com>
2019-03-05 09:12:44 -0500

    Added Scrolling Functionality
    Added scrolling functionality for site using more locations. Adjusted max-height and subtle shadow to indicated the ability to scroll to see more.

--- mobile/index.php
+++ mobile/index.php
@@ -23,69 +23,69 @@ function XMOB_injection()
 	//https://www.advancedcustomfields.com/resources/get_field/
 	
 	$toggle = get_field('enabledisable', 'option');
-		
+			
 	if( $toggle == 'toggle_on') {
   	
   	if( have_rows('mobile_location')) {
     
       $location_repeater = get_field('mobile_location', 'option');
       
-      echo "<div class='innexus-mobile'>";
-      
       echo "<div class='location-toggle'><i class='fas fa-mobile-alt'></i><i class='fas fa-times'></i></div>";
       
-      //Loop thorough each location
-      foreach($location_repeater as $location)
-      {
-        $name = $location['location_name'];
-        $phone = $location['phone_number'];
-        $phonei = $location['phone_icon'];
-        $text = $location['text_number'];
-        $texti = $location['text_icon'];
-        $email = $location['email_address'];
-        $emaili = $location['email_icon'];
-        $phonetoggle = $location['toggle_phone_number'];
-        $texttoggle = $location['toggle_text_number'];
-        $emailtoggle = $location['toggle_email'];
-        
-        //clean the numbers
-        $phone_clean = preg_replace('~[-._#,]~', '', $phone);
-        $text_clean = preg_replace('~[-._#,]~', '', $text);
-        
-        
-        echo "<div class='location-container'>";
-          echo "<p class='location-name'><strong>" . $name . "</strong></p>";
-          echo "<ul class='location-info'>";
-          	
-          	//Controls visibility of Phone Number
-            if( $phonetoggle == 'phone_number_on' ) {
-              echo "<a href='tel:+1" . $phone_clean . "' class='location-phone'>";
-                echo "$phonei";
-                echo "<p>Call</p>";
-              echo "</a>";
-            }
+      echo "<div class='innexus-mobile'>";
             
-            //Controls visibility of Texting Number
-            if( $texttoggle == 'text_number_on') {
-              echo "<a href='sms:" . $text_clean . "' class='location-text'>";
-                echo "$texti";
-                echo "<p>Text</p>";
-              echo "</a>";
-            }
+        //Loop thorough each location
+        foreach($location_repeater as $location)
+        {
+          $name = $location['location_name'];
+          $phone = $location['phone_number'];
+          $phonei = $location['phone_icon'];
+          $text = $location['text_number'];
+          $texti = $location['text_icon'];
+          $email = $location['email_address'];
+          $emaili = $location['email_icon'];
+          $phonetoggle = $location['toggle_phone_number'];
+          $texttoggle = $location['toggle_text_number'];
+          $emailtoggle = $location['toggle_email'];
+          
+          //clean the numbers
+          $phone_clean = preg_replace('~[-._#,]~', '', $phone);
+          $text_clean = preg_replace('~[-._#,]~', '', $text);
+          
+          
+          echo "<div class='location-container'>";
+            echo "<p class='location-name'><strong>" . $name . "</strong></p>";
+            echo "<ul class='location-info'>";
+            	
+            	//Controls visibility of Phone Number
+              if( $phonetoggle == 'phone_number_on' ) {
+                echo "<a href='tel:+1" . $phone_clean . "' class='location-phone'>";
+                  echo "$phonei";
+                  echo "<p>Call</p>";
+                echo "</a>";
+              }
+              
+              //Controls visibility of Texting Number
+              if( $texttoggle == 'text_number_on') {
+                echo "<a href='sms:" . $text_clean . "' class='location-text'>";
+                  echo "$texti";
+                  echo "<p>Text</p>";
+                echo "</a>";
+              }
+              
+              //Controls visibility of Email
+              if( $emailtoggle == 'email_on') {
+                echo "<a href='mailto:" . $email . "' class='location-email'>";
+                  echo "$emaili";
+                  echo "<p>Email</p>";
+                echo "</a>";
+              }
+              
+            echo "</ul>";
+          echo "</div>";
+          
+        }    
             
-            //Controls visibility of Email
-            if( $emailtoggle == 'email_on') {
-              echo "<a href='mailto:" . $email . "' class='location-email'>";
-                echo "$emaili";
-                echo "<p>Email</p>";
-              echo "</a>";
-            }
-            
-          echo "</ul>";
-        echo "</div>";
-        
-      }    
-  
       echo "</div>";
   
   	}
--- mobile/js/script.js
+++ mobile/js/script.js
@@ -5,7 +5,12 @@ jQuery(document).ready(function($)
   if ( $('.innexus-mobile').length ) {
     $('.location-toggle').click(function() {
       $('.innexus-mobile').toggleClass('active');
+      $('.location-toggle').toggleClass('active');
     })
+  }
+  
+  if ( $('.innexus-mobile').outerHeight() >= 420 ) {
+    $('.innexus-mobile').addClass('shadow');
   }
 
 });
