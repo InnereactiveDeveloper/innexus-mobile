6962d98a1a0264921d06914b5201a7e933918cdd
Chris Colvin <chris.colvin@getinnexus.com>
2020-02-26 09:13:46 -0500

    A Few Changes
    Added Mobile/Chatbot switch, added a few button options, and split up styling for easier management

--- mobile/index.php
+++ mobile/index.php
@@ -43,208 +43,234 @@ function XMOB_injection()
 	//if display option is set to mobile or all…
 	if( $display == 'display_mobile' || $display == 'display_all' ) 
 	{
-		//grab the location fields
-		$location_repeater = get_field('mobile_location', 'option');
-		$appointment_request_link_repeater = get_field('appointment_request_link_repeater', 'option');
-		  	
-		//if the plugin display option is *not* set to Off…
-		if($display != 'Off')
-		{
-		
-			//add an open button
-			echo "<div class='location-open $display $leftRight'><img src='http://staging.getinnexus.com/chrissandbox/wp-content/uploads/2020/01/chatbot_f.png' alt='chatbot icon'></div>";
-			
-			//card container
-			echo "<div class='innexus-mobile $display $leftRight'>";
-			  //add a close button
-			  echo "<div class='location-close $display $leftRight'><i class='fas fa-times'></i></div>";
-			  //add the image/icon
-			  echo "<div class='location-chatbot $display $leftRight'><img src='http://staging.getinnexus.com/chrissandbox/wp-content/uploads/2020/01/chatbot_f.png' alt='chatbot icon'></div>";
-			  
-			  //display the first question and options
-			  echo "<div class='chatbot-container $display $leftRight'>";
-			    echo "<p class='chatbot-response'>What can I help you do today?</p>";
-			    
-			    //if showing appointment requests…
-  			  if(in_array('request_appointment', $homeData)) {
-    			  
-    			  //show the appointment button
-    			  echo "<a href='#' class='chatbot-button'>Request an Appointment</a>";
-  			  }
-  			  
-  			  //if showing contact us…
-  			  if(in_array('contact_us', $homeData)) {
-    			  
-    			  //show the contact us button
-    			  echo "<a href='#' class='chatbot-button'>Contact Your Office</a>";
-  			  }
-  			  
-  			  //if showing online patient forms…
-  			  if(in_array('online_patient_forms', $homeData)) {
-    			  
-    			  //show the appointment button
-    			  echo "<a href='#' class='chatbot-button'>Find Patient Forms</a>";
-  			  }
-  			  
-  			  //if showing hours…
-  			  if(in_array('hours', $homeData)) {
-    			  
-    			  //show the appointment button
-    			  echo "<a href='#' class='chatbot-button'>View Your Hours</a>";
-  			  }
-  			  
-  			  //if showing Location…
-  			  if(in_array('practice_location', $homeData)) {
-    			  
-    			  //show the appointment button
-    			  echo "<a href='#' class='chatbot-button'>Find Your Location</a>";
-  			  }
-  			  
-  			  //if showing more options…
-  			  if(in_array('more_options', $homeData)) {
-    			  
-    			  //show the appointment button
-    			  echo "<a href='#' class='chatbot-button'>More Options</a>";
-  			  }
-			  echo "</div>";
-			    
-				//Loop through each location
-/*
-				foreach($location_repeater as $location)
+  	//and the basic version is selected
+  	if($version == 'innexus_mobile') {
+    	
+    	//grab the location fields
+    	$location_repeater = get_field('mobile_location', 'option');
+    	
+    	//Loop through each location
+			foreach($location_repeater as $location)
+			{
+				//variables
+				$name = $location['location_name'];
+        $phone = $location['phone_number'];
+        $phonei = $location['phone_icon'];
+        $text = $location['text_number'];
+        $texti = $location['text_icon'];
+        $email = $location['email_address'];
+        $emaili = $location['email_icon'];
+        $appt = $location['appt_req'];
+        $appti = $location['appt_icon'];
+        $phonetoggle = $location['toggle_phone_number'];
+        $texttoggle = $location['toggle_text_number'];
+        $emailtoggle = $location['toggle_email'];
+        $appttoggle = $location['toggle_appt'];
+        $count = 0;
+        $classes = '';
+        
+        //check if phone toggle is on, count if it is
+        if( $phonetoggle == 'phone_number_on' ) 
 				{
-					//variables
-					$name = $location['location_name'];
-          $phone = $location['phone_number'];
-          $phonei = $location['phone_icon'];
-          $text = $location['text_number'];
-          $texti = $location['text_icon'];
-          $email = $location['email_address'];
-          $emaili = $location['email_icon'];
-          $appt = $location['appt_req'];
-          $appti = $location['appt_icon'];
-          $phonetoggle = $location['toggle_phone_number'];
-          $texttoggle = $location['toggle_text_number'];
-          $emailtoggle = $location['toggle_email'];
-          $appttoggle = $location['toggle_appt'];
-          $count = 0;
-          $classes = '';
-          
-          //check if phone toggle is on, count if it is
-          if( $phonetoggle == 'phone_number_on' ) 
-					{
-            $count++;
-					}
-					
-					//check if texting toggle is on, count if it is
-					if( $texttoggle == 'text_number_on') 
-					{
-            $count++;
-					}
-					
-					//check if email toggle is on, count if it is
-					if( $emailtoggle == 'email_on') 
-					{
-            $count++;
-					}
-					
-					//check if appt toggle is on, count if it is and apply classes based on count
-					if($appttoggle == 'appt_on') 
-					{
-            $count++;
-            
-            if ( $count == 1 ) 
-            {
-    					$classes = 'appt_one';
-  					}
-  					
-  					if ( $count == 2 ) 
-  					{
-    					$classes = 'appt_two';
-  					}
-  					
-  					if ( $count == 3 ) 
-  					{
-    					$classes = 'appt_three';
-  					}
-  					
-  					if ( $count == 4 ) 
-  					{
-    					$classes = 'appt_four';
-  					}
-					} 
-					else
-					//apply classes based on count
-					{
-  					if ( $count == 1 ) 
-  					{
-    					$classes = 'one';
-  					}
-  					
-  					if ( $count == 2 ) 
-  					{
-    					$classes = 'two';
-  					}
-  					
-  					if ( $count == 3 ) 
-  					{
-    					$classes = 'three';
-  					}
-					}
-					
-					//clean the numbers
-					$phone_clean = preg_replace('~[-._#,]~', '', $phone);
-					$text_clean = preg_replace('~[-._#,]~', '', $text);
-					
-					//inner containers and location name
-					echo "<div class='location-container " . $classes . "'>";
-						echo "<p class='location-name'><strong>" . $name . "</strong></p>";
-						echo "<ul class='location-info'>";
-							
-							//Controls visibility of Phone Number
-							if( $phonetoggle == 'phone_number_on' ) 
-							{
-								echo "<li class='location-phone'><a href='tel:+1" . $phone_clean . "'>";
-									echo $phonei;
-									echo "<p>Call</p>";
-								echo "</a></li>";
-							}
-							
-							//Controls visibility of Texting Number
-							if( $texttoggle == 'text_number_on') 
-							{
-								echo "<li class='location-text'><a href='sms:" . $text_clean . "'>";
-									echo $texti;
-									echo "<p>Text</p>";
-								echo "</a></li>";
-							}
-							
-							//Controls visibility of Email
-							if( $emailtoggle == 'email_on') 
-							{
-								echo "<li class='location-email'><a href='mailto:" . $email . "'>";
-									echo $emaili;
-									echo "<p>Email</p>";
-								echo "</a></li>";
-							}
-							
-							//Controls visibility of Appointment Request
-							if( $appttoggle == 'appt_on') 
-							{
-								echo "<li class='location-appt'><a href='" . $appt . "'>";
-									echo $appti;
-									echo "<p>Appointments</p>";
-								echo "</a></li>";
-							}
-						  
-						echo "</ul>";
-					echo "</div>";
-				  
+          $count++;
 				}
-*/    
-			    
-			echo "</div>";
-		
-		}
+				
+				//check if texting toggle is on, count if it is
+				if( $texttoggle == 'text_number_on') 
+				{
+          $count++;
+				}
+				
+				//check if email toggle is on, count if it is
+				if( $emailtoggle == 'email_on') 
+				{
+          $count++;
+				}
+				
+				//check if appt toggle is on, count if it is and apply classes based on count
+				if($appttoggle == 'appt_on') 
+				{
+          $count++;
+          
+          if ( $count == 1 ) 
+          {
+  					$classes = 'appt_one';
+					}
+					
+					if ( $count == 2 ) 
+					{
+  					$classes = 'appt_two';
+					}
+					
+					if ( $count == 3 ) 
+					{
+  					$classes = 'appt_three';
+					}
+					
+					if ( $count == 4 ) 
+					{
+  					$classes = 'appt_four';
+					}
+				} 
+				else
+				//apply classes based on count
+				{
+					if ( $count == 1 ) 
+					{
+  					$classes = 'one';
+					}
+					
+					if ( $count == 2 ) 
+					{
+  					$classes = 'two';
+					}
+					
+					if ( $count == 3 ) 
+					{
+  					$classes = 'three';
+					}
+				}
+				
+				//clean the numbers
+				$phone_clean = preg_replace('~[-._#,]~', '', $phone);
+				$text_clean = preg_replace('~[-._#,]~', '', $text);
+				
+				//inner containers and location name
+				echo "<div class='location-toggle $display $leftRight'><i class='fas fa-mobile-alt'></i><i class='fas fa-times'></i></div>";
+				
+				echo "<div class='innexus-mobile $display $leftRight'>";
+  				echo "<div class='location-container " . $classes . "'>";
+  					echo "<p class='location-name'><strong>" . $name . "</strong></p>";
+  					echo "<ul class='location-info'>";
+  						
+  						//Controls visibility of Phone Number
+  						if( $phonetoggle == 'phone_number_on' ) 
+  						{
+  							echo "<li class='location-phone'><a href='tel:+1" . $phone_clean . "'>";
+  								echo $phonei;
+  								echo "<p>Call</p>";
+  							echo "</a></li>";
+  						}
+  						
+  						//Controls visibility of Texting Number
+  						if( $texttoggle == 'text_number_on') 
+  						{
+  							echo "<li class='location-text'><a href='sms:" . $text_clean . "'>";
+  								echo $texti;
+  								echo "<p>Text</p>";
+  							echo "</a></li>";
+  						}
+  						
+  						//Controls visibility of Email
+  						if( $emailtoggle == 'email_on') 
+  						{
+  							echo "<li class='location-email'><a href='mailto:" . $email . "'>";
+  								echo $emaili;
+  								echo "<p>Email</p>";
+  							echo "</a></li>";
+  						}
+  						
+  						//Controls visibility of Appointment Request
+  						if( $appttoggle == 'appt_on') 
+  						{
+  							echo "<li class='location-appt'><a href='" . $appt . "'>";
+  								echo $appti;
+  								echo "<p>Appointments</p>";
+  							echo "</a></li>";
+  						}
+  					  
+  					echo "</ul>";
+  				echo "</div>";
+        echo "</div>";			  
+			}
+  	}
+  	
+  	//but if the chatbot version is selected
+  	if($version == 'innexus_chatbot') {
+  	
+  		//grab the location fields
+  		$location_repeater_chatbot = get_field('mobile_location_chatbot', 'option');
+  		$intro = get_field('intro', 'option');
+  		  	
+  		//if the plugin display option is *not* set to Off…
+  		if($display != 'Off')
+  		{
+  		
+  			//add an open button
+  			echo "<div class='chatbot-open $display $leftRight'><img src='http://staging.getinnexus.com/chrissandbox/wp-content/uploads/2020/01/chatbot_f.png' alt='chatbot icon'></div>";
+  			
+  			//card container
+  			echo "<div class='innexus-chatbot $display $leftRight'>";
+  			  //add a close button
+  			  echo "<div class='chatbot-close $display $leftRight'><i class='fas fa-times'></i></div>";
+  			  //add the image/icon
+  			  echo "<div class='chatbot-icon $display $leftRight'><img src='http://staging.getinnexus.com/chrissandbox/wp-content/uploads/2020/01/chatbot_f.png' alt='chatbot icon'></div>";
+  			  
+  			  //display the first question and options
+  			  echo "<div class='chatbot-container $display $leftRight'>";
+  			    echo "<p class='chatbot-response'>$intro</p>";
+  			    
+  			    foreach($location_repeater_chatbot as $location) {
+    			    $name = $location['location_name_chatbot'];
+    			    
+    			    //if showing appointment requests…
+      			  if(in_array('request_appointment', $homeData)) {
+        			  $apptLink = $location['appt_req_chatbot'];
+        			  $apptCopy = $location['appointment_request_button_copy'];
+        			  
+        			  //show the appointment button
+        			  echo "<a href='$apptLink' class='chatbot-button'>$apptCopy</a>";
+      			  }
+      			  
+      			  //if showing contact us…
+      			  if(in_array('contact_us', $homeData)) {
+        			  $contactLink = $location['contact_us_chatbot'];
+        			  $contactCopy = $location['contact_us_button_copy'];
+        			  
+        			  //show the contact us button
+        			  echo "<a href='$contactLink' class='chatbot-button'>$contactCopy</a>";
+      			  }
+      			  
+      			  //if showing online patient forms…
+      			  if(in_array('online_patient_forms', $homeData)) {
+        			  $formsLink = $location['patient_forms'];
+        			  $formsCopy = $location['patient_forms_button_copy'];
+        			  
+        			  //show the appointment button
+        			  echo "<a href='$formsLink' class='chatbot-button'>$formsCopy</a>";
+      			  }
+      			  
+      			  //if showing hours…
+      			  if(in_array('hours', $homeData)) {
+        			  //$hours = $location['patient_forms_chatbot'];
+        			  $hoursCopy = $location['office_hours_button_copy'];
+        			  
+        			  //show the appointment button
+        			  echo "<a href='#' class='chatbot-button hours $name'>$hoursCopy</a>";
+      			  }
+      			  
+      			  //if showing Location…
+      			  if(in_array('practice_location', $homeData)) {
+        			  $locationLink = $location['office_location_link'];
+        			  $locationCopy = $location['office_location_button_copy'];
+        			  
+        			  //show the appointment button
+        			  echo "<a href='$locationLink' class='chatbot-button'>$locationCopy</a>";
+      			  }
+      			  
+      			  //if showing more options…
+      			  if(in_array('more_options', $homeData)) {
+        			  
+        			  //show the appointment button
+        			  echo "<a href='#' class='chatbot-button'>More Options</a>";
+      			  }
+  			    }
+  			    
+  			  echo "</div>";			    
+  			echo "</div>";
+  		
+  		}
+    }
 	}
 }
 
--- mobile/js/script.js
+++ mobile/js/script.js
@@ -2,33 +2,43 @@
 jQuery(document).ready(function($)
 {
   //if the mobile plugin exists…
-  if ( $('.innexus-mobile').length ) {
+  if ( $('.innexus-chatbot').length ) {
     
     //when the open button is clicked…
-    $('.location-open').click(function() {
+    $('.chatbot-open').click(function() {
       
       //add the active class on the button and plugin container
-      $('.innexus-mobile').addClass('active');
-      $('.location-open').addClass('active');
-      $('.location-close').addClass('active');
+      $('.innexus-chatbot').addClass('active');
+      $('.chatbot-open').addClass('active');
+      $('.chatbot-close').addClass('active');
     })
     
     //when the close button is clicked…
-    $('.location-close').click(function() {
+    $('.chatbot-close').click(function() {
       
       //add the active class on the button and plugin container
-      $('.innexus-mobile').removeClass('active');
-      $('.location-open').removeClass('active');
-      $('.location-close').removeClass('active');
+      $('.innexus-chatbot').removeClass('active');
+      $('.chatbot-open').removeClass('active');
+      $('.chatbot-close').removeClass('active');
+    })
+  }
+  
+  //if the mobile plugin exists…
+  if ( $('.innexus-mobile').length ) {
+    
+    //when the toggle button is click…
+    $('.location-toggle').click(function() {
+      
+      //toggle the active class on the button and plugin container
+      $('.innexus-mobile').toggleClass('active');
+      $('.location-toggle').toggleClass('active');
     })
   }
   
   //if the height of the container is equal to or greater than 420px…
-/*
   if ( $('.innexus-mobile').outerHeight() >= 420 ) {
     
     //add the shadow class to the container
     $('.innexus-mobile').addClass('shadow');
   }
-*/
 });
