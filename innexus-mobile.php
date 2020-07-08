51f819f217ed17c3964d8c366c3886d9b6bde939
Chris Colvin <chris.colvin@getinnexus.com>
2020-07-08 10:25:27 -0400

    Update 2.0.1
    update 2.0.1

--- innexus-mobile.php
+++ innexus-mobile.php
@@ -10,7 +10,7 @@
  * Description: Innexus Mobile
  * Author: Innexus by Innereactive
  * Author URI: https://www.getinnexus.com
- * Version: 2.0.0
+ * Version: 2.0.1
  */
 
 // Exit if accessed directly.
